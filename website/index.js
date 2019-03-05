981f07d5c792a8a2ff831e30ba491a55edf7ef88
ChaseManseth <chasemanseth@gmail.com>
2019-03-04 20:48:25 -0600

    Test

--- website/index.js
+++ website/index.js
@@ -3,19 +3,21 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
-    res.send("Hello World!");
+    res.send("Hello Worl123123d!");
 });
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "node",
-  password: "chase"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "node",
+//   password: "chase"
+// });
+//
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-app.listen(3000, () => console.log('Listening on port 3000....'));
+app.listen(port, () => console.log(`Listening on port ${port}....`));

b02daf5ccde555b1922e4acc605ee8085cf2d66d
ChaseManseth <chasemanseth@gmail.com>
2019-04-09 23:53:26 -0500

    Fixed Connection

--- BicepBuddyAPI/app.js
+++ BicepBuddyAPI/app.js
@@ -9,19 +9,11 @@ const userRoutes = require('./api/routes/user');
 // Delete this when in production
 const mongodbPW = "uXwozBcr3tFtWnbI";
 // Delete this when in production
-
-const uri = "mongodb+srv://Manseth:' + mongodbPW + '@bicepbuddy-gosub.mongodb.net/test?retryWrites=true";
-const mongoose = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-// mongoose.connect('mongodb+srv://Manseth:' + mongodbPW + '@bicepbuddy-gosub.mongodb.net/test?retryWrites=true',
-//     {
-//         useNewUrlParser: true
-//     }
-// );
+mongoose.connect('mongodb+srv://Manseth:' + mongodbPW + '@bicepbuddy-gosub.mongodb.net/test?retryWrites=true&authSource=admin',
+    {
+        useNewUrlParser: true
+    }
+);
 
 mongoose.Promise = global.Promise;
 
